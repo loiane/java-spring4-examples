b192cfccff06c6b40dc69c81c0aeee602b281ffc
Loiane Groner <loianeg@gmail.com>
2015-08-22 13:51:36 -0300

    spring4-rest-hello

---
+++ spring4-rest-hello/src/main/java/com/loiane/config/HelloWorldConfig.java
@@ -0,0 +1,12 @@
+package com.loiane.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.loiane")
+public class HelloWorldConfig {
+
+}
---
+++ spring4-rest-hello/src/main/java/com/loiane/config/HelloWorldInit.java
@@ -0,0 +1,28 @@
+package com.loiane.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class HelloWorldInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { HelloWorldConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+}
